a6118e3911206ffb79c9dc8b09738536ab0010b1
Moloh258 <123gada321@gmail.com>
2024-04-07 23:11:01 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("Введите выражение в формате X + Y")
+		inputText, _ := reader.ReadString('\n')
+		inputText = strings.TrimSpace(inputText)
+		splittedInputText := strings.Split(inputText, " ")
+
+		if len(splittedInputText) == 1 {
+			fmt.Println("Выдача паники, так как строка не является математической операцией.")
+			continue
+		}
+
+		if len(splittedInputText) != 3 {
+			fmt.Println("Выдача паники, так как формат математической операции не удовлетворяет заданию — два операнда и один оператор (+, -, /, *).")
+			continue
+		}
+
+		firstNumber := parseNumber(splittedInputText[0])
+		secondNumber := parseNumber(splittedInputText[2])
+		operator := splittedInputText[1]
+
+		if firstNumber == 0 || secondNumber == 0 {
+			fmt.Println("Ошибка: неверный формат числа")
+			continue
+		}
+
+		if isRomanNumeral(splittedInputText[0]) != isRomanNumeral(splittedInputText[2]) {
+			fmt.Println("Выдача паники, так как используются одновременно разные системы счисления.")
+			continue
+		}
+
+		result := 0
+		switch operator {
+		case "+":
+			result = firstNumber + secondNumber
+		case "-":
+			result = firstNumber - secondNumber
+		case "*":
+			result = firstNumber * secondNumber
+		case "/":
+			result = firstNumber / secondNumber
+		default:
+			fmt.Println("Какой-то не такой у вас оператор")
+			continue
+		}
+
+		if isRomanNumeral(splittedInputText[0]) {
+			romanResult := arabicToRoman(result)
+			if result < 0 {
+				fmt.Println("Выдача паники, так как в римской системе нет отрицательных чисел.")
+			} else {
+				fmt.Println("Результат: ", romanResult)
+			}
+		} else {
+			fmt.Println("Результат: ", result)
+		}
+	}
+}
+
+func parseNumber(number string) int {
+	arabicNumber, err := strconv.Atoi(number)
+	if err == nil && arabicNumber >= 0 && arabicNumber <= 10 {
+		return arabicNumber
+	} else {
+		romanMap := map[string]int{"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10}
+		arabicNumber, isRoman := romanMap[number]
+		if isRoman {
+			return arabicNumber
+		}
+	}
+	return 0
+}
+
+func isRomanNumeral(input string) bool {
+	roman := []string{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"}
+	for _, numeral := range roman {
+		if input == numeral {
+			return true
+		}
+	}
+	return false
+}
+
+func arabicToRoman(num int) string {
+	if num <= 0 || num > 10 {
+		return ""
+	}
+
+	romanNumerals := []string{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"}
+	return romanNumerals[num-1]
+}
